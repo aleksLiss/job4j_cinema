d3b281f404fd8807ff21a0d0641567d5e666df12
aleksei <darklisik.darklisik@yandex.ru>
2025-02-13 22:20:23 +0300

    create dml script for fill out table 'file'

---
+++ db/scripts/dml/008_dml_insert_files.sql
@@ -0,0 +1,7 @@
+insert into files(name, path) values ('97045', 'files/97045.jpg');
+insert into files(name, path) values ('124361', 'files/124361.jpg');
+insert into files(name, path) values ('257362', 'files/257362.jpg');
+insert into files(name, path) values ('458171', 'files/458171.jpg');
+insert into files(name, path) values ('1488316', 'files/1488316.jpg');
+insert into files(name, path) values ('2023303', 'files/2023303.jpg');
+insert into files(name, path) values ('6173994253', 'files/6173994253.jpg');
