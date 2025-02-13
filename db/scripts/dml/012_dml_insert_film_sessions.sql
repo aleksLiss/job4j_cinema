9b0feb6fe3e61ed4ee2a062c3eff99c06be1253b
aleksei <darklisik.darklisik@yandex.ru>
2025-02-13 22:21:12 +0300

    create dml script for fill out table 'film_sessions'

---
+++ db/scripts/dml/012_dml_insert_film_sessions.sql
@@ -0,0 +1,7 @@
+insert into film_sessions(film_id,halls_id,start_time,end_time,price) values(1, 1, '2025-01-15 16:00:00', '2025-01-15 17:43:10', 400);
+insert into film_sessions(film_id,halls_id,start_time,end_time,price) values(2, 2, '2025-01-18 13:00:00', '2025-01-18 15:43:10', 360);
+insert into film_sessions(film_id,halls_id,start_time,end_time,price) values(3, 2, '2025-01-21 12:00:00', '2025-01-21 13:41:15', 410);
+insert into film_sessions(film_id,halls_id,start_time,end_time,price) values(4, 2, '2025-01-23 17:00:00', '2025-01-23 17:47:00', 200);
+insert into film_sessions(film_id,halls_id,start_time,end_time,price) values(5, 1, '2025-01-25 18:00:00', '2025-01-25 20:02:30', 340);
+insert into film_sessions(film_id,halls_id,start_time,end_time,price) values(6, 2, '2025-01-27 14:00:00', '2025-01-27 16:30:15', 450);
+insert into film_sessions(film_id,halls_id,start_time,end_time,price) values(7, 2, '2025-01-30 15:00:00', '2025-01-30 17:49:40', 430);
