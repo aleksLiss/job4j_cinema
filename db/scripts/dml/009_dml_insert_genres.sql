502e23b9ccdb2458690cb707964cbc09f32f741e
aleksei <darklisik.darklisik@yandex.ru>
2025-02-13 22:20:41 +0300

    create dml script for fill out table 'genres'

---
+++ db/scripts/dml/009_dml_insert_genres.sql
@@ -0,0 +1,5 @@
+insert into genres(name) values('боевик');
+insert into genres(name) values('художественный');
+insert into genres(name) values('комедия');
+insert into genres(name) values('триллер');
+insert into genres(name) values('научная фантастика');
