d79f79cd3c026ddf26bdf3027bab56e85dd7dd0c
aleksei <darklisik.darklisik@yandex.ru>
2025-02-19 17:14:20 +0300

    redact methods 'findById' and 'create' into GenreRepository.java: add Optional

--- src/main/java/ru/job4j/cinema/repository/GenreRepository.java
+++ src/main/java/ru/job4j/cinema/repository/GenreRepository.java
@@ -3,16 +3,17 @@ package ru.job4j.cinema.repository;
 import ru.job4j.cinema.model.Genre;
 
 import java.util.Collection;
+import java.util.Optional;
 
 public interface GenreRepository {
 
-    Genre create(Genre genre);
+    Optional<Genre> create(Genre genre);
 
     boolean update(Genre genre);
 
     boolean deleteById(int id);
 
-    Genre findById(int id);
+    Optional<Genre> findById(int id);
 
     Collection<Genre> findAll();
 }
