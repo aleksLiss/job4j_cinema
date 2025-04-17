72d9854d8be7327b76aab8b6c3ba17469f38cc5b
aleksei <darklisik.darklisik@yandex.ru>
2025-04-18 00:58:55 +0300

    add 003_dml_insert_genres.sql

---
+++ src/main/resources/db/scripts/003_dml_insert_genres.sql
@@ -0,0 +1,4 @@
+insert into genres(name) values ('action');
+insert into genres(name) values ('Science fiction');
+insert into genres(name) values ('horror');
+insert into genres(name) values ('comedy');
