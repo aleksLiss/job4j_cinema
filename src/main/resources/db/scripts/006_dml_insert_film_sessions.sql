8679ce18c442a3c5338a836f31fca64a5137d2eb
aleksei <darklisik.darklisik@yandex.ru>
2025-04-18 00:59:15 +0300

    add 006_dml_insert_film_sessions.sql

---
+++ src/main/resources/db/scripts/006_dml_insert_film_sessions.sql
@@ -0,0 +1,4 @@
+insert into film_sessions(film_id, halls_id, start_time, end_time, price) values (1, 1, '2025-04-10 16:40:50', '2025-04-10 19:20:50', 5);
+insert into film_sessions(film_id, halls_id, start_time, end_time, price) values (2, 1, '2025-04-15 18:20:30', '2025-04-15 20:01:30', 14);
+insert into film_sessions(film_id, halls_id, start_time, end_time, price) values (3, 2, '2025-04-13 20:00:00', '2025-04-13 21:37:00', 3);
+insert into film_sessions(film_id, halls_id, start_time, end_time, price) values (4, 2, '2025-04-11 16:50:00', '2025-04-11 18:44:00', 4);
