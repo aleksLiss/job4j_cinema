c84ea26c6e110143ee9ebb3aeb4660509ace0a9f
aleks <darklissik.darklissik@gmail.com>
2025-07-17 23:23:59 +0300

    add AuthorizationFilter.java

---
+++ src/main/java/ru/job4j/cinema/filter/AuthorizationFilter.java
@@ -0,0 +1,42 @@
+package ru.job4j.cinema.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.core.annotation.Order;
+
+@Component
+@Order(1)
+public class AuthorizationFilter extends HttpFilter {
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String uri = request.getRequestURI();
+        if (isAlwaysPermitted(uri)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        boolean userLoggedIn = request.getSession().getAttribute("user") != null;
+        if (!userLoggedIn) {
+            String loginPageUrl = request.getContextPath() + "/users/login";
+            response.sendRedirect(loginPageUrl);
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+
+    private boolean isAlwaysPermitted(String uri) {
+        return uri.startsWith("/users/register") ||
+                uri.startsWith("/users/login") ||
+                uri.startsWith("/js") ||
+                uri.startsWith("/css") ||
+                uri.startsWith("/sessions") ||
+                uri.startsWith("/films");
+    }
+}
